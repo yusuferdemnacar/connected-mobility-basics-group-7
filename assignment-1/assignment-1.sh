e0510f450e8f1b083a19b94f59a874d8283064a3
yusuferdemnacar <ysuuferdemnacar@gmail.com>
2025-06-05 13:59:11 +0200

    Add a running script and update readme

---
+++ assignment-1/assignment-1.sh
@@ -0,0 +1,17 @@
+python schedule-gen/main.py \
+    --the-one-dir the-one \
+    --data-dir the-one/data \
+    --map-dir the-one/data/fmi-map \
+    --corridor-file-path the-one/data/fmi-map/corridor.wkt \
+    --settings-file-path the-one/assignment-1-settings.txt \
+    --nrof-courses 5 \
+    --nrof-lt-groups 5 \
+    --nrof-hosts-per-lt-group 3
+
+cd the-one
+
+./compile.sh
+
+# list the settings file here
+./one.sh \
+    ./assignment-1-settings.txt
