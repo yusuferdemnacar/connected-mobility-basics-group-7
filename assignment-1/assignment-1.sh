2b2140191b89ca0f2667913b8cfead9fcc10eb3a
Dan Bachar <dan@bachar.co>
2025-06-10 23:20:24 +0200

    generate different schedule for each run

--- assignment-1/assignment-1.sh
+++ assignment-1/assignment-1.sh
@@ -1,36 +1,37 @@
-python3 schedule-gen/main.py \
-    --the-one-dir the-one \
-    --data-dir the-one/data \
-    --map-dir the-one/data/fmi-map \
-    --corridor-file-path the-one/data/fmi-map/corridor.wkt \
-    --settings-file-path the-one/assignment-1-settings.txt \
-    --nrof-courses 5 \
-    --nrof-lecture-taker-groups 5 \
-    --nrof-hosts-per-lecture-taker-group 3 \
-    --nrof-self-studier-hosts 5 \
+#!/bin/bash
 
-cd the-one
-
-./compile.sh
-
+cd the-one && ./compile.sh
+cd -
 # Run multiple simulations with different message sizes
-for size in 100 1000 10000 100000 1000000 5000000; do
+for size in 100 1000; do
+    sed -e "s/Events1.size = .*/Events1.size = $size/" \
+            the-one/assignment-1-settings-communications.txt > "the-one/assignment-1-settings-communications-${size}.txt"
     echo "Running simulations for message size: $size"
-    
-    # Run multiple simulations for each message size
-    for run in $(seq 1 100); do
-        echo "  Running simulation $run/1 for size $size"
-        
+    for run in $(seq 1 1); do
         # Create temporary config file with unique scenario name for each run
         sed -e "s/Scenario.name = .*/Scenario.name = CMB_${size}_run${run}/" \
-            assignment-1-settings.txt > "assignment-1-settings-${size}.txt"
-        sed -e "s/Events1.size = .*/Events1.size = $size/" \
-            assignment-1-settings-communications.txt > "assignment-1-settings-communications-${size}.txt"
+            the-one/assignment-1-settings.txt > "the-one/assignment-1-settings-${size}-${run}.txt"
+        
+        # Generate schedule per run and message size 
+        python3 schedule-gen/main.py \
+            --the-one-dir the-one \
+            --data-dir the-one/data \
+            --map-dir the-one/data/fmi-map \
+            --corridor-file-path the-one/data/fmi-map/corridor.wkt \
+            --settings-file-path the-one/assignment-1-settings-${size}-${run}.txt \
+            --nrof-courses 5 \
+            --nrof-lecture-taker-groups 5 \
+            --nrof-hosts-per-lecture-taker-group 3 \
+            --nrof-self-studier-hosts 5 \
+
+        cd the-one
+        echo "  Running simulation $run/1 for size $size"
         
         # Run the simulation
         ./one.sh -b 1 \
-            "./assignment-1-settings-${size}.txt" \
-            "./assignment-1-settings-communications-${size}.txt"
+            "assignment-1-settings-${size}-${run}.txt" \
+            "assignment-1-settings-communications-${size}.txt"
+        cd -
     done
     
     echo "Completed all 1 simulations for message size: $size"
---
+++ assignment-1/the-one/src/report/MessageHopDeliveryReport.java
@@ -0,0 +1,2 @@
+package report;public class MessageHopDeliveryReport {
+}
